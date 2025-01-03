56163096b3a519e912500782bd335bf462fef128
Tejas-Shukla-0512 <tejasshuklazm05@gmail.com>
2025-01-03 22:05:06 +0530

    update

---
+++ 20-Day20-Functions.py
@@ -0,0 +1,7 @@
+# def matlab define a function
+def plus(a,b): # plus is a function name for a and b
+  add=a+b  # add matlab funtion ka kam a+b (add aap ka variable hai)
+  print(add) # ye print kar dega 
+x= int(input("enter 1st number"))
+y= int(input("enter 2nd number"))
+plus(x,y)
